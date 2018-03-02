0cf848af0b7660ba79d972486914339c183be0ee
xcalibur839 <NA>
2018-03-02 06:39:43 -0800

    Fixed chart loading issue with simple error handling

--- charts.js
+++ charts.js
@@ -1,39 +1,44 @@
 google.charts.load('current', {packages: ['corechart', 'line']});
 
 function drawChart() {
-    for (var i = 0; i < WalletArray.length; i++) {
-        var data = new google.visualization.DataTable();
-        data.addColumn('datetime', 'Time');
-        data.addColumn('number', 'Current');
-        data.addColumn('number', 'Reported');
-        data.addColumn('number', 'Average');
+    try {
+        for (var i = 0; i < WalletArray.length; i++) {
+            var data = new google.visualization.DataTable();
+            data.addColumn('datetime', 'Time');
+            data.addColumn('number', 'Current');
+            data.addColumn('number', 'Reported');
+            data.addColumn('number', 'Average');
 
-        var options = {
-            title: WalletArray[i].name,
-            hAxis: {
-                title: 'Time',
-                baselineColor: 'red'
-            },
-            vAxis: {
-                title: 'Hashrate (Mh/s)'
-            },
-            legend: { 
-                position: 'bottom',
-                display: 'none'
-            },
-            explorer: {}
-        };
+            var options = {
+                title: WalletArray[i].name,
+                hAxis: {
+                    title: 'Time',
+                    baselineColor: 'red'
+                },
+                vAxis: {
+                    title: 'Hashrate (Mh/s)'
+                },
+                legend: { 
+                    position: 'bottom',
+                    display: 'none'
+                },
+                explorer: {}
+            };
 
-        for (var j = 0; j < WalletArray[i].history.length; j ++) {
-            data.addRow([
-                new Date(WalletArray[i].history[j].time * timeOffset), 
-                WalletArray[i].history[j].currentHashrate / mHashOffset,
-                WalletArray[i].history[j].reportedHashrate / mHashOffset,
-                WalletArray[i].history[j].averageHashrate / mHashOffset
-            ]);
+            for (var j = 0; j < WalletArray[i].history.length; j ++) {
+                data.addRow([
+                    new Date(WalletArray[i].history[j].time * timeOffset), 
+                    WalletArray[i].history[j].currentHashrate / mHashOffset,
+                    WalletArray[i].history[j].reportedHashrate / mHashOffset,
+                    WalletArray[i].history[j].averageHashrate / mHashOffset
+                ]);
+            }
+
+            var chart = new google.visualization.LineChart(document.getElementById(WalletArray[i].name));
+            chart.draw(data, options);
         }
-
-        var chart = new google.visualization.LineChart(document.getElementById(WalletArray[i].name));
-        chart.draw(data, options);
+    }
+    catch (e) {
+        console.log("Error loading chart\n" + e);
     }
 }
